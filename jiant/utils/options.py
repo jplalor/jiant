ed631a5507fb027702f4618d04de5a2922f59408
John Lalor <john.lalor@nd.edu>
2019-11-06 22:36:28 +0800

    bugfix

--- jiant/utils/options.py
+++ jiant/utils/options.py
@@ -4,6 +4,8 @@ Functions for parsing configs.
 import torch
 import logging as log
 
+from tasks import ALL_GLUE_TASKS, ALL_SUPERGLUE_TASKS
+
 
 def parse_task_list_arg(task_list):
     """Parse task list argument into a list of task names."""
