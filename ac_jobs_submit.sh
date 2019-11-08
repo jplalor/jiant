3f39da6198137d98933949c4abaf9a77a515cc7e
John Lalor <lalor@cs.umass.edu>
2019-11-07 19:46:37 -0500

    update

--- ac_jobs_submit.sh
+++ ac_jobs_submit.sh
@@ -2,5 +2,5 @@
 
 for p in $(seq 0.02 0.02 1.0)
 do 
-sbatch --output=/mnt/nfs/work1/hongyu/lalor/data/jiant/logs/ac_$p.txt --export p=$p ac_job.slurm 
+sbatch --output=/mnt/nfs/work1/hongyu/lalor/data/jiant/logs/ac-$p.txt --export p=$p ac_job.slurm 
 done 
